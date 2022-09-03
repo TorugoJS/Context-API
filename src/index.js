8c942554c95531e58a6226c82b39882333d60ccd
TorugoJS <victor.devfrontend@gmail.com>
2022-09-03 17:15:45 -0300

    context

--- src/index.js
+++ src/index.js
@@ -4,12 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { CounterContextProvider } from './context/CounterContext';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    {/* 2 - criando provider */}
+    <CounterContextProvider>
 
-    <App />
+      <App />
+    </CounterContextProvider>
   </React.StrictMode>
 );
 
---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import { useContext } from "react"
+
+import { CounterContext } from "../context/CounterContext"
+
+const Home = () => {
+
+    const {counter} = useContext(CounterContext)
+
+  return (
+    <div>
+        <h1>Home</h1>
+        <p>Valor do contador: {counter}</p>
+    </div>
+  )
+}
+
+export default Home
